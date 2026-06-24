3648989e12e9d3f7fab227977b7da5b049063977
Bryan Chavajay <bchavajaydev@gmail.com>
2026-06-23 22:49:10 -0600

    add entities

---
+++ BryShort.Core/Entities/Link.cs
@@ -0,0 +1,15 @@
+﻿using BryShort.Core.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Core.Entities;
+
+public class Link
+{
+    public int Id { get; private set; }
+    public string ShortUrl { get; private set; } = default!;
+    public Url UrlTo { get; private set; } = default!;
+    public int UserId { get; private set; }
+    public bool IsDeleted { get; private set; }
+}
---
+++ BryShort.Core/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Core.Entities;
+
+public class User
+{
+    public int Id {  get; private set; }
+    public Guid PublicId { get; private set; } = Guid.CreateVersion7();
+    public string Username { get; private set; } = default!;
+    public string Password { get; private set; } = default!;
+    public bool IsActive { get; private set; } = true;
+    public bool IsDeleted { get; private set; } = false;
+}
