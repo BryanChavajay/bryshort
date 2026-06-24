0deb368fa5b27c20cc41473a6b89a04491b26f4a
Bryan Chavajay <bchavajaydev@gmail.com>
2026-06-24 17:10:49 -0600

    Objetos de valor agregados

--- BryShort.Core/ValueObjects/Url.cs
+++ BryShort.Core/ValueObjects/Url.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BryShort.Core.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +9,24 @@ public class Url
 {
 
     public string Value { get; private set; } = default!;
-    public DateTime? Expires_at { get; private set; } = null;
+    public DateTime? ExpiresAt { get; private set; } = null;
 
+    private Url (string value, DateTime? expiresAt)
+    {
+        if (string.IsNullOrEmpty(value)) { throw new BusinessRuleException("La url a redirigir no puede estar vacia"); }
+
+        Value = value;
+        ExpiresAt = expiresAt;
+    }
+
+    public static Url Create(string value, DateTime? expiresAt)
+    {
+        return new Url(value, expiresAt);
+    }
+
+    public bool IsValid()
+    {
+        if (ExpiresAt is null) { return true; }
+        return ExpiresAt <= DateTime.UtcNow;
+    }
 }
