8d9dcdb157a67de576c9e184b35194ca7a29103d
Bryan Chavajay <bchavajaydev@gmail.com>
2026-06-23 22:49:31 -0600

    add base exception

---
+++ BryShort.Core/Exceptions/BaseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Core.Exceptions;
+
+internal class BaseException : Exception
+{
+    public string StatusCode { get; set; } = default!;
+
+    public BaseException(string message) : base(message)
+    {
+    }
+}
