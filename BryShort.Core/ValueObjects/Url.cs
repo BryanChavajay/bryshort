c6f873153c8a55cb458ba320a42d5c6d250a171f
Bryan Chavajay <bchavajaydev@gmail.com>
2026-06-23 22:49:02 -0600

    Add value object

---
+++ BryShort.Core/ValueObjects/Url.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Core.ValueObjects;
+
+public class Url
+{
+
+    public string Value { get; private set; } = default!;
+    public DateTime? Expires_at { get; private set; } = null;
+
+}
