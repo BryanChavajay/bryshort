8218bf9e8eb14dd577b241e6e5a60af8771acc97
Bryan Chavajay <bchavajaydev@gmail.com>
2026-06-24 17:10:21 -0600

    Entidades agregadas

--- BryShort.Core/Entities/Link.cs
+++ BryShort.Core/Entities/Link.cs
@@ -1,4 +1,5 @@
-﻿using BryShort.Core.ValueObjects;
+﻿using BryShort.Core.Exceptions;
+using BryShort.Core.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,4 +13,26 @@ public class Link
     public Url UrlTo { get; private set; } = default!;
     public int UserId { get; private set; }
     public bool IsDeleted { get; private set; }
+
+    private Link(int id, string shortUrl, Url urlTo,  int userId,  bool isDeleted)
+    {
+        if (string.IsNullOrWhiteSpace(shortUrl)) { throw new BusinessRuleException("La url corta no puede ser vacia");  }
+        if (shortUrl.Contains(' ')) { throw new BusinessRuleException("La url corta no puede contener espacios en blanco"); }
+
+        Id = id;
+        ShortUrl = shortUrl;
+        UrlTo = urlTo;
+        UserId = userId;
+        IsDeleted = isDeleted;
+    }
+
+    static public Link Create(string shortUrl, Url urlTo, int userId, bool isDeleted, int id = 0)
+    {
+        return new Link(0, shortUrl, urlTo, userId, isDeleted);
+    }
+
+    static public Link LoadToRepository(int id, string shortUrl, Url urlTo, int userId, bool isDeleted)
+    {
+        return new Link(id, shortUrl, urlTo, userId, isDeleted);
+    }
 }
--- BryShort.Core/Entities/User.cs
+++ BryShort.Core/Entities/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BryShort.Core.Exceptions;
 
 namespace BryShort.Core.Entities;
 
@@ -12,4 +13,29 @@ public class User
     public string Password { get; private set; } = default!;
     public bool IsActive { get; private set; } = true;
     public bool IsDeleted { get; private set; } = false;
+
+    private User(int id, Guid publicId, string username, string password, bool isActive, bool isDeleted)
+    {
+        if (string.IsNullOrWhiteSpace(username)) { throw new BusinessRuleException("El nombre de usuario es obligatorio"); }
+        if (string.IsNullOrWhiteSpace(password)) { throw new BusinessRuleException("La contrasenia debe es obligatorioa"); }
+
+        Id = id;
+        PublicId = publicId;
+        Username = username;
+        Password = password;
+        IsActive = isActive;
+        IsDeleted = isDeleted; 
+    }
+
+    public static User Create(string username, string password, bool isActive)
+    {
+        if (password.Length < 8) { throw new BusinessRuleException("La contraseña no debe ser menor a 8"); }
+        if (username.Contains(' ')) { throw new BusinessRuleException("El nombre de usuario no debe contener espacios en blanco"); }
+        return new User( 0, Guid.CreateVersion7(), username, password, isActive, false );
+    }
+
+    public static User LoadToRepository(int id, Guid publicId, string username, string password, bool isActive, bool isDeleted)
+    {
+        return new User(id, publicId, username, password, isActive, isDeleted);
+    }
 }
