8c585dee423c2907830b7a68bbfea0762748fc16
Bryan Chavajay <bchavajaydev@gmail.com>
2026-06-24 17:12:15 -0600

    Excepcion Regla de Negocio agregada

---
+++ BryShort.Core/Exceptions/BusinessRuleException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Core.Exceptions;
+
+public class BusinessRuleException : BaseException
+{
+    public new const string StatusCode = "400";
+
+    public BusinessRuleException(string message) : base(message, StatusCode)
+    {
+    }
+}
