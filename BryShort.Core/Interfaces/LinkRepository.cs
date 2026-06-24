444cdc83bc7d6f981ce5322defc36f337fdf5d26
Bryan Chavajay <bchavajaydev@gmail.com>
2026-06-24 17:12:42 -0600

    Interfaces agregadas

---
+++ BryShort.Core/Interfaces/LinkRepository.cs
@@ -0,0 +1,13 @@
+﻿using BryShort.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Core.Interfaces;
+
+public interface ILinkRepository
+{
+    Task<Link> Add(Link link);
+
+    Task<Link> GetByShortUrl(string shortUrl);
+}
---
+++ BryShort.Core/Interfaces/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using BryShort.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Core.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User> Add(User user);
+
+    Task<User> GetById(int id);
+
+    Task<User> GetByPublicId(Guid publicId);
+
+    Task<User> GetByUsername(string username);
+
+    Task<bool> ExistUsername(string username);
+}
