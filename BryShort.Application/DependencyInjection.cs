9e17dfeadf32bce4eed9dbb0fa3d95237c3ec50d
Bryan Chavajay <bchavajaydev@gmail.com>
2026-07-01 16:19:14 -0600

    add: inyeccion de dependencias para los casos de uso

---
+++ BryShort.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using BryShort.Application.Links.Create;
+using BryShort.Application.Users.Create;
+using BryShort.Application.Utils.Mediator;
+using BryShort.Core.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Application;
+
+public static class DependyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddTransient<IMediator, SimpleMediator>();
+
+        services.AddScoped<IRequestExecute<CreateUserCommand, User>, UseCaseCreateUser>();
+        services.AddScoped<IRequestExecute<CreateLinkCommand, Link>, UseCaseCreateLink>();
+
+        return services;
+    }
+}
