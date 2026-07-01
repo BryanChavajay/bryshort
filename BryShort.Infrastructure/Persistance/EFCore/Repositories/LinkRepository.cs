7db6315b9266f9cd0e86f349ce6a8dad138c5a77
Bryan Chavajay <bchavajaydev@gmail.com>
2026-07-01 16:20:22 -0600

    add: repositorios para EFCore

---
+++ BryShort.Infrastructure/Persistance/EFCore/Repositories/LinkRepository.cs
@@ -0,0 +1,23 @@
+﻿using BryShort.Core.Entities;
+using BryShort.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Infrastructure.Persistance.EFCore.Repositories;
+
+internal class EFLinkRepository(ApplicationDbContext context) : ILinkRepository
+{
+    public async Task<Link> Add(Link link)
+    {
+        context.Add(link);
+        await context.SaveChangesAsync();
+        return link;
+    }
+
+    public async Task<Link?> GetByShortUrl(string shortUrl)
+    {
+        var link = context.Links.OrderByDescending(x => x.Id).FirstOrDefault(x => x.ShortUrl == shortUrl);
+        return link;
+    }
+}
---
+++ BryShort.Infrastructure/Persistance/EFCore/Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using BryShort.Core.Entities;
+using BryShort.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Infrastructure.Persistance.EFCore.Repositories;
+
+internal class EFUserRepository(ApplicationDbContext context) : IUserRepository
+{
+    public async Task<User> Add(User user)
+    {
+        context.Add(user);
+        await context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<bool> ExistUsername(string username)
+    {
+        return await context.Users.AnyAsync(x => x.Username == username);
+    }
+
+    public async Task<User?> GetById(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        return user;
+    }
+
+    public async Task<User?> GetByPublicId(Guid publicId)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x => x.PublicId == publicId);
+        return user;
+    }
+
+    public async Task<User?> GetByUsername(string username)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x => x.Username == username);
+        return user;
+    }
+}
