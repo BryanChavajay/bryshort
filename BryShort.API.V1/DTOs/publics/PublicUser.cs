f9ecb567969d0b09ecdd4d04861d1582de4542ea
Bryan Chavajay <bchavajaydev@gmail.com>
2026-07-28 23:08:26 -0600

    add: se crearon dtos para usuarios

---
+++ BryShort.API.V1/DTOs/publics/PublicUser.cs
@@ -0,0 +1,6 @@
+﻿namespace BryShort.API.V1.DTOs.publics;
+
+public record PublicUser(
+    Guid Id,
+    string Username
+);
---
+++ BryShort.API.V1/DTOs/requests/CreateUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BryShort.API.V1.DTOs.requests;
+
+public class CreateUserDTO
+{
+    [Required]
+    [MaxLength(64)]
+    public required string UserName { get; set; }
+    [Required]
+    [MaxLength(32)]
+    public required string Password { get; set; }
+}
