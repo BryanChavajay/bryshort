7072a8a7d7367d6ffb3165b7a4eab51023ed6f85
Bryan Chavajay <bchavajaydev@gmail.com>
2026-09-04 16:00:35 -0600

    Add: error no encontrado

---
+++ BryShort.Core/Exceptions/NotFoundError.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Core.Exceptions;
+
+public class NotFoundError(string message): BaseException(message, statusCode: 404)
+{
+}
