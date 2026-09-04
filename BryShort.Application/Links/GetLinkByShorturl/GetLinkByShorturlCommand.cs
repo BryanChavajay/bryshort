f90c3a7112af7ba2578fc1c88da3e559b719fec7
Bryan Chavajay <bchavajaydev@gmail.com>
2026-09-04 16:01:24 -0600

    add: comando y caso de uso para busqueda de url

---
+++ BryShort.Application/Links/GetLinkByShorturl/GetLinkByShorturlCommand.cs
@@ -0,0 +1,11 @@
+﻿using BryShort.Application.Utils.Mediator;
+using BryShort.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Application.Links.GetLinkByShorturl;
+
+public record GetLinkByShorturlCommand(
+    string ShortUrl
+) : IRequest<Link>;
---
+++ BryShort.Application/Links/GetLinkByShorturl/UseCaseGetLinkByShorturl.cs
@@ -0,0 +1,22 @@
+﻿using BryShort.Application.Utils.Mediator;
+using BryShort.Core.Entities;
+using BryShort.Core.Exceptions;
+using BryShort.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BryShort.Application.Links.GetLinkByShorturl;
+
+public class UseCaseGetLinkByShorturl(ILinkRepository linkRepository)
+    : IRequestExecute<GetLinkByShorturlCommand, Link>
+{
+    public async Task<Link> Execute(GetLinkByShorturlCommand request)
+    {
+        var link = await linkRepository.GetByShortUrl(request.ShortUrl);
+
+        if (link == null || !link.UrlTo.IsValid()) { throw new NotFoundError("Link no encontrado o expirado"); }
+        
+        return link;
+    }
+}
